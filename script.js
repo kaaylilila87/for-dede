c79d1225a06eda44cfe8fb014c9715cd7dcfd7b5
kaaylilila87 <kaylila81008@gmail.com>
2026-02-20 15:44:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+
+const text = "aya sayang banget sama dede, aya minta maaf yaaa sayanggg :>";
+const loveText = document.getElementById("loveText");
+
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    loveText.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 80);
+  }
+}
+
+setTimeout(() => {
+  loveText.style.opacity = "1";
+  typeWriter();
+}, 1000);
